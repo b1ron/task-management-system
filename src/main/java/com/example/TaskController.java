8f460e8743d03c0e980ee04440ce9304d3ba8830
b1ron <goldingbyron@gmail.com>
2025-02-12 15:16:29 +0100

    mock a task

--- src/main/java/com/example/TaskController.java
+++ src/main/java/com/example/TaskController.java
@@ -1,12 +1,19 @@
 package com.example;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
-@RequestMapping("/tasks")
 public class TaskController {
 
- // ...
+    private List<Task> mock = Arrays.asList(new Task("1", "P2", false));
+
+    @GetMapping("/tasks")
+    public List<Task> get() {
+        return mock;
+    }
 
 }
