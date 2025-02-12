b0e698cbeebeed319e578807c8b1e08fe0ef3fd8
b1ron <goldingbyron@gmail.com>
2025-02-12 22:03:39 +0100

    add mappings

--- src/main/java/com/example/TaskController.java
+++ src/main/java/com/example/TaskController.java
@@ -1,19 +1,33 @@
 package com.example;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 public class TaskController {
 
-    private List<Task> mock = Arrays.asList(new Task("1", "P2", false));
+    private Map<String, Task> mock = new HashMap<String, Task>() {{
+        put("1", new Task("1", "P1", false));
+        put("2", new Task("2", "P2", false));
+    }};
 
     @GetMapping("/tasks")
-    public List<Task> get() {
-        return mock;
+    public Collection<Task> get() {
+        return mock.values();
+    }
+
+    @GetMapping("/tasks/{id}")
+    public Task get(@PathVariable String id) {
+        Task task = mock.get(id);
+        if (task == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        return task;
     }
 
 }
