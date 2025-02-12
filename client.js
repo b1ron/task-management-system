a670ecc7e4951798a9426c919a66fe60716f93a9
b1ron <goldingbyron@gmail.com>
2025-02-12 15:26:39 +0100

    add a fetch call

---
+++ client.js
@@ -0,0 +1,10 @@
+(async function createTask() {
+    let task = { title: '2', priority: 'P1', completed: false };
+    await fetch('/http://localhost:8080/tasks', {
+        method: 'POST',
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+        },
+    })
+})();
