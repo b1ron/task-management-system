f656b65f7557ff353ec798b02606b1c3c39a7835
b1ron <goldingbyron@gmail.com>
2025-02-22 06:43:09 +0100

    remove unused imports

--- src/main/java/com/example/controller/TaskController.java
+++ src/main/java/com/example/controller/TaskController.java
@@ -7,12 +7,9 @@ import com.example.exception.TaskNotFoundException;
 import com.example.model.Task;
 import com.example.service.TaskService;
 
-import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
